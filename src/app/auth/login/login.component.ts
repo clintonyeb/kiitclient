c8e9828d1a54b09f9f5543df79a28a86570e449b
clintonyeb <clintonmethis@gmail.com>
2017-01-03 03:44:51 +0530

    Taking some rest

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from "../../services/user.service";
+import {AuthService} from "../../services/auth.service";
+import {userInfo} from "os";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(public userService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  doLogin(){
+    this.userService.login('clinton', 'password').subscribe(data => console.log(data), err => console.error(err));
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,23 +1,68 @@
 import { Injectable } from '@angular/core';
-import {Headers} from "@angular/http";
+import {Headers, Http, Response, RequestOptions, RequestOptionsArgs} from "@angular/http";
+import {Observable} from "rxjs";
+import {BASE_URL} from "./user.service";
+import {User} from "../models/user";
+import 'rxjs/add/operator/map'
 
 @Injectable()
 export class AuthService {
 
-  constructor() { }
+  currentUser: User;
+
+  constructor(public http: Http) { }
+
+  login(username: string, password: string): Observable<boolean>{
+     return this.http.post(`${BASE_URL}/login`,
+      JSON.stringify({username: username, password: password}),
+      this.getBasicHeaders())
+      .map((response) => {
+      console.log(response);
+        return response.json() as User
+      })
+      .map((user) =>{
+        this.currentUser = user;
+        this.setToken(user.username, user.token);
+
+        if (this.currentUser){
+          console.log('authenticated success');
+          return true;
+        }
+        else {
+          console.error('authenticated failed');
+          return false;
+        }
+      });
+  }
 
   isLoggedIn(): boolean {
     return  !!this.getToken()
   }
 
+  register(){}
+
   getToken(): string {
     return localStorage.getItem('token');
   }
 
-  getHeaders(): Headers{
-    let header: Headers;
-    header.append('')
-    return header;
+  setToken(username: string, token: string){
+    localStorage.setItem('token', JSON.stringify({username: username, token: token}));
+  }
+
+  getBasicHeaders(): RequestOptions{
+    let header = new  Headers({'Content-Type': 'application/json'});
+    header.append('Accept', 'application.json');
+    return new RequestOptions({headers: header});
+  }
+
+  getAuthenticatedHeader(){
+    let token: string;
+    if (token = this.getToken()){
+      let header = new  Headers({'Content-Type': 'application/json'});
+      header.append('Accept', 'application.json');
+      header.append('X-Auth-Token', `${token}`);
+      return new RequestOptions({headers: header});
+    }
   }
 
 }
